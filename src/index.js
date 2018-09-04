20ae4cda7df092df97bfa62b0b6de49fc6dd0cf2
Sourav Das <sourav.iss2015@gmail.com>
2018-09-04 18:12:59 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 // import numeral from 'numeral';
 var numeral=require('numeral');
 // import numeral from 'numeral';
-
 /* eslint-disable no-console */
 const courseValue=numeral(1000).format('$0,0.00');
 // console.log('I would pay '+courseValue+' for this awesome course');
